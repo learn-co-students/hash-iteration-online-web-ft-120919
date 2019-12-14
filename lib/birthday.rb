42abb2699cf4305abd044fae03690b17d89007d6
github username <github email address>
2019-12-14 15:09:11 -0500

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
@@ -5,7 +7,20 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age| #key, value
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+
+  end
 end
 
 
+def age_appropriate_birthday(birthday_kids)
+  #iterate over each of the key value pair
+  birthday_kids.each do |name, age|
+    # binding.pry
+  end
+  #check if age <= 12
+
+  #puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  #puts "You are too old for this.""
+end
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,3 +1,4 @@
+
 # passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
@@ -7,5 +8,71 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  #return the name of the passenger who wins
+  winner = ""
+  #iterate through each key in passengers hash
+  passengers.each do |suite, name|
+    #boolean goes to the key suite_a, and finds the value that starts w/letter A
+    if suite == :suite_a && name.start_with?("A") 
+      #sets winner variable = to that name
+      winner = name
+    end
+  end
+  #return the winner
+  winner
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def select_winner(passengers)
+#   winner = "" #create a winner variable = to a string to store the value we want
+#   passengers.each do |suite, name| #|key, value| #use each method on hash
+#     if suite == :suite_a && name.start_with?("A") # if the block suite is equal to the key of suite_a, and name value starts with A
+#       winner = name #then the variable winner equals the value of that name block that starts with A
+#     end #if
+#   end #do
+#   winner #and returns that value to terminal
+# end #def
+# #binding.pry
